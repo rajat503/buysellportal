329df46098ed9b49c1761bf4af0023a7453030aa
Rajat Agarwal <rajat503@gmail.com>
2014-08-03 13:32:48 +0530

    Updated registration and options

--- options.php
+++ options.php
@@ -3,11 +3,10 @@ require 'core.php';
 		if(loggedin())
 		{
 
-		echo '<a href="userhome.php">My Courses</a><br/>';	
-		echo '<a href="addcourse.php">Add a course</a><br/>';
-		echo '<a href="deletecourse.php">Delete a course</a><br/>';
+		echo '<a href="userhome.php">Home</a><br/>';	
+		echo '<a href="addtosell.php">Sell</a><br/>';
+		echo '<a href="searchbyproduct.php">Search by Product</a><br/>';
 		echo '<a href="searchbyid.php">Seach by ID Number</a><br/>';
-		echo '<a href="searchbycourse.php">Search By Course </a><br/>';
 		echo '<a href="searchbyname.php">Search By Name </a><br/>';
 		echo '<a href="logout.php">Log Out </a><br/>';
 	}
--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 require 'connect.php';
 require 'core.php';
-if(isset($_POST['id']) && isset($_POST['password']) && isset($_POST['name']) && isset($_POST['contactnumber']))
+if(isset($_POST['id']) && isset($_POST['password']) && isset($_POST['name']) && isset($_POST['contactnumber']) && isset($_POST['hostel']) && isset($_POST['room']))
 {
 	$id=$_POST['id'];
 	$id = strtoupper($id);
@@ -9,7 +9,9 @@ if(isset($_POST['id']) && isset($_POST['password']) && isset($_POST['name']) && 
 	$name=$_POST['name'];
 	$name = strtoupper($name);
 	$contactnumber=$_POST['contactnumber'];
-	if(!empty($id) && !empty($password) && !empty($name) && !empty($contactnumber))
+	$hostel=$_POST['hostel'];
+	$room=$_POST['room'];
+	if(!empty($id) && !empty($password) && !empty($name) && !empty($contactnumber) && !empty($room) && !empty($hostel))
 	{
 		$query="SELECT `serial` FROM `users` WHERE `id`='$id'";
 		if($query_run=mysql_query($query))
@@ -17,11 +19,11 @@ if(isset($_POST['id']) && isset($_POST['password']) && isset($_POST['name']) && 
 			$query_num_rows=mysql_num_rows($query_run);
 			if($query_num_rows!=0)
 			{
-				echo "ID already registered.";
+				echo "ID already registered. Please report to the admin if you did not register before.";
 			}
 			else
 			{
-				$query="INSERT INTO `users` VALUES ('','".$id."','".$name."','".$password."','".$contactnumber."')";
+				$query="INSERT INTO `users` VALUES ('','".$id."','".$name."','".$password."','".$contactnumber."','".$hostel."','".$room."')";
 				if($query_run=mysql_query($query))
 				{
 					
@@ -43,11 +45,33 @@ if(isset($_POST['id']) && isset($_POST['password']) && isset($_POST['name']) && 
 	}
 	else
 	{
-		echo "You must supply an ID and password";
+		echo "You must enter all fields.";
 	}
 }
 ?>
 <form action="<?php echo $current_file; ?>" method="POST">
-Enter you Complete BITS ID: <input type="text" name="id"> Name: <input type="text" name="name"> Desired Password: <input type="password" name="password"> Contact No: <input type="text" name="contactnumber">
+Enter you Complete BITS ID: <input type="text" name="id"> <br/>
+Name: <input type="text" name="name"><br/>
+Desired Password: <input type="password" name="password"> <br/>
+Contact No: <input type="text" name="contactnumber"> <br/> 
+Hostel: 
+<select name="hostel">
+	<option value="">Select</option>
+	<option value="AH1">AH1</option>
+	<option value="AH2">AH2</option>
+	<option value="AH3">AH3</option>
+	<option value="AH4">AH4</option>
+	<option value="AH5">AH5</option>
+	<option value="AH6">AH6</option>
+	<option value="AH7">AH7</option>
+	<option value="AH8">AH8</option>
+	<option value="CH1">CH1</option>
+	<option value="CH2">CH2</option>
+	<option value="CH3">CH3</option>
+	<option value="CH4">CH4</option>
+	<option value="CH5">CH5</option>
+	<option value="CH6">CH6</option>
+</select> <br/>
+Room No: <input type="text" name="room"> <br/>
 <input type="submit" value="Register">
 </form>
