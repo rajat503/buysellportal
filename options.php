da424d22e54f42d48c0c875e2400f3953553aa5e
Rajat Agarwal <rajat503@gmail.com>
2014-08-03 18:34:24 +0530

    Options revised
    Different searches truncated,

--- options.php
+++ options.php
@@ -5,9 +5,7 @@ require 'core.php';
 
 		echo '<a href="userhome.php">Home</a><br/>';	
 		echo '<a href="selladd.php">Sell</a><br/>';
-		echo '<a href="buybybranch.php">Search by Product</a><br/>';
-		echo '<a href="searchbyid.php">Seach by ID Number</a><br/>';
-		echo '<a href="searchbyname.php">Search By Name </a><br/>';
+		echo '<a href="buybybranch.php">Buy</a><br/>';
 		echo '<a href="logout.php">Log Out </a><br/>';
 	}
 else
